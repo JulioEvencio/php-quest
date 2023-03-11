28a408e03b34c92f24cf2e7428bd73c83ffa60b4
JulioEvencio <julioevencio@acad.ifma.edu.br>
2023-03-11 16:25:04 -0300

    refactoring logic

--- Application.php
+++ Application.php
@@ -4,10 +4,18 @@
 
         public function execute() {
             $url = isset($_GET['url']) ? explode('/', $_GET['url'])[0] : 'Home';
+            $url = ucfirst($url);
+            $url .= 'Controller';
 
-            switch ($url) {
-                default:
-                    echo 'Não encontrado';
+            if (file_exists('./app/controllers/'.$url.'.php')) {
+                require_once('./app/controllers/'.$url.'.php');
+
+                $controller = new $url();
+                $controller->execute();
+            } else {
+                require_once('./app/controllers/HomeController.php');
+                $controller = new HomeController();
+                $controller->execute();
             }
         }
 
