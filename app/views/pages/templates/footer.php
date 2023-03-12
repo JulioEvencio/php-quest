9ffa221664995ab1ad644146e862fc3de97eeb97
JulioEvencio <julioevencio@acad.ifma.edu.br>
2023-03-12 14:12:07 -0300

    Changing the project architecture

--- app/views/pages/templates/footer.php
+++ app/views/pages/templates/footer.php
@@ -6,6 +6,6 @@
         </div>
     </footer>
 
-    <script src="./app/views/pages/js/script.js"></script>
+    <script src="./public/js/script.js"></script>
 </body>
 </html>
--- app/views/pages/templates/header.php
+++ app/views/pages/templates/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./app/views/pages/css/style.css">
-    <link rel="stylesheet" href="./app/views/pages/css/header.css">
-    <link rel="stylesheet" href="./app/views/pages/css/footer.css">
+    <link rel="stylesheet" href="./public/css/style.css">
+    <link rel="stylesheet" href="./public/css/header.css">
+    <link rel="stylesheet" href="./public/css/footer.css">
     <title>PHP Quest</title>
 </head>
 <body>
